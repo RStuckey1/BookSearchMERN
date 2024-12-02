7c2f7619e748140cd59293875a10e400998fe4c1
RStuckey1 <stuckey85@gmail.com>
2024-12-02 16:58:12 -0600

    render config

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-dotenv.config();
+require("dotenv").config();
 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks';
 
